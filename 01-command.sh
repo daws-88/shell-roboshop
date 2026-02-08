d04670ad8bad4f3ced3c8c9b851da6867dccac85
Venkat <psai96526@gmail.com>
2026-02-08 18:37:24 +0530

    new

--- 01-command.sh
+++ 01-command.sh
@@ -5,7 +5,7 @@ SG_ID="sg-00234e667183faf3a"
 
 for instance in $@
 do
-    INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text)
 
 
     if [ $instance != "frontend" ]; then
