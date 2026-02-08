394dababd034904e4c94e165d89224961c4e8898
Venkat <psai96526@gmail.com>
2026-02-08 16:19:17 +0530

    new

--- 01-command.sh
+++ 01-command.sh
@@ -5,19 +5,17 @@ SG_ID="sg-00234e667183faf3a"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro 
-    --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
-    Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$($ aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output.txt)
+
 
     if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances
-        [0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID     --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances
-        [0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID    --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+
     fi
     echo "$instance: $IP"
-done
+    done
 
 
 
