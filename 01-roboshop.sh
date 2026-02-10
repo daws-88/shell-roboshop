e08551d7c0750cddc700a8586f6d5e927413e9bf
Venkat <psai96526@gmail.com>
2026-02-10 22:05:34 +0530

    new

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -7,13 +7,13 @@ DOMAIN_NAME="daws88s.fun"
 
 for instance in $@
 do
-   INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Test}]" --query 'Instances[0].InstanceId' --output text)
+   INSTANCE_ID=$( aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Test}]" --query 'Instances[0].InstanceId' --output text )
    # get private IP
-   if [ $instance != "frontend"]; then
-       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+   if [ $instance != "frontend" ]; then
+       IP=$( aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
        RECORD_NAME="$instance.$DOMAIN_NAME"
    else
-       IP=$(aws ec2 describe-instances --instance-ids i-0682bd1dbd92ce16c --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+       IP=$( aws ec2 describe-instances --instance-ids i-0682bd1dbd92ce16c --query 'Reservations[0].Instances[0].PublicIpAddress' --output text )
        RECORD_NAME="$DOMAIN_NAME"
    fi
 
