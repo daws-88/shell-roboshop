669d2831c4bc8cb5f31ac2d29afe5ccb9d8a1a52
Venkat <psai96526@gmail.com>
2026-02-11 10:54:45 +0530

    new

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -6,7 +6,7 @@ DOMAIN_NAME="daws88s.fun"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5  --instance-type t3.micro --security-group-ids sg-00234e667183faf3a  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_BAME"
