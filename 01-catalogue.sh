e738a36d30671f825ad425b89d8d07b103494988
Venkat <psai96526@gmail.com>
2026-02-12 15:35:57 +0530

    new

--- 01-catalogue.sh
+++ 01-catalogue.sh
@@ -76,7 +76,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "install mongodb client"
 
 INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then
+if [ $INDEX -eq 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "load products"
 else
