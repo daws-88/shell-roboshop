a3aac96a0db11d85642cecbbc76bed3d38d65c55
Venkat <psai96526@gmail.com>
2026-02-13 19:51:37 +0530

    new

--- 01-rabbitmq.sh
+++ 01-rabbitmq.sh
@@ -1,48 +1,51 @@
+
 #!/bin/bash
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-SCRIPT_DIR=$PWD
-USERID=$(id -u)
-LOG_FOLDER="/var/log/shell-script"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOG_FOLDER
-echo "Script strated at $(date)" | tee -a $LOG_FILE
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+SCRIPT_DIR=$PWD
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script as root privelliage"
-    exit 1
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE() {
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ....$R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
-    else 
-        echo -e "$2....$G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-###### RABBITMQ ########
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
-VALIDATE $? "Adding mongo.repo"
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VALIDATE $? "Adding RabbitMQ repo"
 
 dnf install rabbitmq-server -y &>>$LOG_FILE
-VALIDATE $? "Install rabbitmq"
+VALIDATE $? "Installing RabbitMQ Server"
 
 systemctl enable rabbitmq-server &>>$LOG_FILE
-VALIDATE  "Enable rabbitmq"
+VALIDATE $? "Enabling RabbitMQ Server"
 
 systemctl start rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Start rabbitmq"
+VALIDATE $? "Starting RabbitMQ"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 VALIDATE $? "Setting up permissions"
 
-systemctl restart rabbitmq
-VALIDATE $? "Restarting rabbitmq"
+
 
 
 
