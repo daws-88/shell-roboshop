aee0d859aa133f7ceffaee5ffa6b5a188b95cb88
Venkat <psai96526@gmail.com>
2026-02-13 18:53:24 +0530

    new

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -25,14 +25,14 @@ VALIDATE() {
 
 ###### MYSQL ######
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOG_FILE
 VALIDATE $? "Install mysql"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOG_FILE
 VALIDATE $? "Enable mysql"
 
-systemctl start mysqld  
+systemctl start mysqld  &>>$LOG_FILE
 VALIDATE $? "start mysql"
 
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
 VALIDATE $? "Setting up root password'
