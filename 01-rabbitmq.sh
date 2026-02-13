fb80ba123ef1b8eecc193841260158ffdf886be2
Venkat <psai96526@gmail.com>
2026-02-13 20:42:55 +0530

    new

--- 01-rabbitmq.sh
+++ 01-rabbitmq.sh
@@ -1,52 +1,45 @@
-
 #!/bin/bash
-
-USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-LOGS_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
-mkdir -p $LOGS_FOLDER
-SCRIPT_DIR=$PWD
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
-
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOG_FOLDER
+echo "Script strated at $(date)" | tee -a $LOG_FILE
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
+    echo "ERROR:: Please run this script as root privelliage"
+    exit 1
 fi
 
-VALIDATE(){ # functions receive inputs through args just like shell script args
+VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ....$R FAILURE $N" | tee -a $LOG_FILE
         exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2....$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-
-cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
-VALIDATE $? "Adding RabbitMQ repo"
+#### RABBITMQ #####
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VALIDATE $? "Copying mongo.repo"
 
 dnf install rabbitmq-server -y &>>$LOG_FILE
-VALIDATE $? "Installing RabbitMQ Server"
+VALIDATE $? "Install rabbitmq"
 
 systemctl enable rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Enabling RabbitMQ Server"
+VALIDATE $? "Enable rabbitmq"
 
 systemctl start rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Starting RabbitMQ"
+VALIDATE $? "Strat rabbitmq"
 
 rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
-VALIDATE $? "Setting up permissions"
+VALIDATE $? "Setting up permessions"
 
-
-
-
-
-
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "script executed in: $TOTAL_TIME inseconds"
